82a05406d0ff601324e2de0c07091c9864bda9b8
Itskarajov15 <asen__7@abv.bg>
2022-11-03 11:24:30 +0200

    Fixed the structure of the project and changed the favicon

--- SharedTrip.Infrastructure/Data/ApplicationDbContext.cs
+++ SharedTrip.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace SharedTrip.Data
+namespace SharedTrip.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- SharedTrip/Program.cs
+++ SharedTrip/Program.cs
@@ -1,10 +1,9 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using SharedTrip.Data;
+using SharedTrip.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -16,15 +15,13 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
