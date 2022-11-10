af889683b5dadbb4b43108e69bc3e1878e1cbf8b
Itskarajov15 <asen__7@abv.bg>
2022-11-10 11:05:15 +0200

    Adjust elements in home page

---
+++ SharedTrip/wwwroot/js/increasingNumbers.js
@@ -0,0 +1,22 @@
+﻿var speed = 10;
+
+function incEltNbr() {
+    eltUsers = document.getElementById('users');
+    endUsers = Number(document.getElementById('users').innerHTML);
+    incNbrRec(0, endUsers, eltUsers);
+
+    eltTrips = document.getElementById('trips');
+    endTrips = Number(document.getElementById('trips').innerHTML);
+    incNbrRec(0, endTrips, eltTrips);
+}
+
+function incNbrRec(i, endNbr, elt) {
+    if (i <= endNbr) {
+        elt.innerHTML = i;
+        setTimeout(function () {
+            incNbrRec(i + 1, endNbr, elt);
+        }, speed);
+    }
+}
+
+window.onload = incEltNbr();
