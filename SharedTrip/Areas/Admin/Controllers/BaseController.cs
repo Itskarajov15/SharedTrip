1d22dd3d16c49419579d6f4d21788cfe5c8c42d3
Itskarajov15 <asen__7@abv.bg>
2022-11-28 13:18:48 +0200

    Added Admin area

---
+++ SharedTrip/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using static SharedTrip.Infrastructure.Data.Constants.AdminConstants;
+
+namespace SharedTrip.Areas.Admin.Controllers
+{
+    [Authorize(Roles = AdministratorRole)]
+    [Area("Admin")]
+    public class BaseController : Controller
+    {
+    }
+}
--- SharedTrip/Program.cs
+++ SharedTrip/Program.cs
@@ -48,9 +48,19 @@ app.UseAuthorization();
 app.SeedAdministrator();
 app.UseNotyf();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+
+    endpoints.MapControllerRoute(
+     name: "default",
+     pattern: "{controller=Home}/{action=Index}/{id?}"
+   );
+
+    app.MapRazorPages();
+});
 
 app.Run();
