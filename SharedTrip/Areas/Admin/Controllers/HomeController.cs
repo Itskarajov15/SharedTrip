7277738ef3b1c2936e74a365bba785969c564bc8
Itskarajov15 <asen__7@abv.bg>
2022-11-29 13:03:26 +0200

    Added try catch block and notyf service to home controller in admin area

--- SharedTrip/Areas/Admin/Controllers/HomeController.cs
+++ SharedTrip/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
 using SharedTrip.Core.Contracts;
 using SharedTrip.Core.Models.Home;
 
@@ -10,27 +11,37 @@ namespace SharedTrip.Areas.Admin.Controllers
         private readonly ITripService tripService;
         private readonly ICarService carService;
         private readonly ICommentService commentService;
+        private readonly INotyfService notyfService;
 
         public HomeController(
             IUserService userService,
             ITripService tripService,
             ICarService carService,
-            ICommentService commentService)
+            ICommentService commentService,
+            INotyfService notyfService)
         {
             this.userService = userService;
             this.tripService = tripService;
             this.carService = carService;
             this.commentService = commentService;
+            this.notyfService = notyfService;
         }
 
         public async Task<IActionResult> Index()
         {
             var model = new DashboardViewModel();
 
-            model.CountOfUsers = await this.userService.GetCountOfUsersAsync();
-            model.CountOfTrips = await this.tripService.GetCountOfTripsAsync();
-            model.CountOfCars = await this.carService.GetCountOfCarsAsync();
-            model.CountOfComments = await this.commentService.GetCountOfCommentsAsync();
+            try
+            {
+                model.CountOfUsers = await this.userService.GetCountOfUsersAsync();
+                model.CountOfTrips = await this.tripService.GetCountOfTripsAsync();
+                model.CountOfCars = await this.carService.GetCountOfCarsAsync();
+                model.CountOfComments = await this.commentService.GetCountOfCommentsAsync();
+            }
+            catch (Exception)
+            {
+                this.notyfService.Error("Something went wrong");
+            }
 
             return View(model);
         }
