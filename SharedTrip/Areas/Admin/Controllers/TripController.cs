ab407228b3156687d4ffc58f9eb91d511a205a4c
Itskarajov15 <asen__7@abv.bg>
2022-12-02 15:32:46 +0200

    Added functionality for all trips in admin area

---
+++ SharedTrip/Areas/Admin/Controllers/TripController.cs
@@ -0,0 +1,45 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using SharedTrip.Core.Contracts;
+using SharedTrip.Core.Models.Trip.ServiceModels;
+
+namespace SharedTrip.Areas.Admin.Controllers
+{
+    public class TripController : BaseController
+    {
+        private readonly ITripService tripService;
+        private readonly INotyfService notyfService;
+
+        public TripController(
+            ITripService tripService,
+            INotyfService notyfService)
+        {
+            this.tripService = tripService;
+            this.notyfService = notyfService;
+        }
+
+        public async Task<IActionResult> All([FromQuery]AllTripsQueryModel query)
+        {
+            try
+            {
+                var result = await this.tripService.AllAsync(
+                query.StartDestinationId,
+                query.EndDestinationId,
+                query.Date,
+                query.CurrentPage,
+                AllTripsQueryModel.TripsPerPage);
+
+                query.TotalTripsCount = result.TotalTripsCount;
+                query.Trips = result.Trips;
+
+                return View(query);
+            }
+            catch (Exception)
+            {
+                this.notyfService.Error("Something went wrong");
+                //add logging
+                return RedirectToAction("Index", "Home", new { area = "Admin" });
+            }
+        }
+    }
+}
